93d569d4747f1c61e6b3f69e07a057e5ab5d3882
AnkitGeorge <ankit.george@gmail.com>
2016-01-15 01:35:11 -0500

    Add Person, Instructor and Student classes.

--- person.rb
+++ person.rb
@@ -1,5 +1,18 @@
+class Person
+  attr_accessor :name
+
+  def greeting
+    "Hi! My name is #{name}"
+  end
+end
+
 class Student
+  def method
+    "I get it!"
+  end
 end
 
 class Instructor
+  def teach
+    "Everything in Ruby is an object."
 end
