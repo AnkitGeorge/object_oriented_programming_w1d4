fd696e364b8bd4c1957ca19e78bd0cd63b94b57e
AnkitGeorge <ankit.george@gmail.com>
2016-01-15 02:56:11 -0500

    Complete Rover class details.

--- mars_rover_AG.rb
+++ mars_rover_AG.rb
@@ -20,13 +20,46 @@ class Rover
     elsif @direction == "E"
       @x += @x
     else
-      puts "ERROR in input."
+      puts "ERROR."
     end
 
   end   #Logic for modifying coordinates based on direction.
 
+  def turn(n_direction)
 
+    case n_direction
+    when "L"
+      if @direction == "N"
+        @direction = "W"
+      elsif @direction == "W"
+        @direction = "S"
+      elsif @direction == "S"
+        @direction = "E"
+      elsif @direction == "E"
+        @direction = "N"
+      else
+        puts "Not a valid input for direction"
+      end
 
+    when "R"
+      if @direction == "N"
+        @direction = "E"
+      elsif @direction == "E"
+        @direction = "S"
+      elsif @direction == "S"
+        @direction = "W"
+      elsif @direction == "W"
+        @direction = "N"
+      else
+        puts "not a direction"
+      end #This end closes if statement
 
+    end   #This end closes case statement.
 
-end
+  end     #This end closes turn method.
+
+  def position
+    puts "#{x}, #{y}, #{direction}"
+  end
+
+end       #This end closes Rover class details.
