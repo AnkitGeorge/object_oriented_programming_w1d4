7e1107743b203cd3482fd14e37f3852bc465c3fb
AnkitGeorge <ankit.george@gmail.com>
2016-01-15 02:39:02 -0500

    Create initialize and move_river methods in Rover class.

--- mars_rover_AG.rb
+++ mars_rover_AG.rb
@@ -4,7 +4,29 @@ class Rover
   def initialize(x , y, direction)
     @x = x.to_i
     @y = y.to_i
-    @direction = direction.upcase
-  end
+    @direction = direction.to_s
+    @direction = @direction.upcase
+  end   #Assigns values to three states of rover.
+
+
+  def move_rover
+
+    if @direction == "N"
+      @y += @y
+    elsif @direction == "W"
+      @x -= @x
+    elsif @direction == "S"
+      @y -= @y
+    elsif @direction == "E"
+      @x += @x
+    else
+      puts "ERROR in input."
+    end
+
+  end   #Logic for modifying coordinates based on direction.
+
+
+
+
 
 end
